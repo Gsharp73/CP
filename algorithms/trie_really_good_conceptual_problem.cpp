2eb2101f181bb897275dbfbe9e6782935e333edb
Dhyanendraa2004 <tripathidhyanendra2004@gmail.com>
2024-07-15 14:22:28 +0530

    added a stag conceptual TRIE problem

---
+++ algorithms/trie_really_good_conceptual_problem.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h> 
+using namespace std;
+struct Node {
+    Node* links[26];
+    int endwith = 0; // number of words that end here
+    int cntpref = 0; // number of words with till this node as prefix 
+    bool containsref(char ch) {
+        return links[ch - 'a'] != NULL;
+    }
+    void putref(char ch, Node* ref) {
+        links[ch - 'a'] = ref;
+    }
+    Node* getref(char ch) {
+        return links[ch - 'a'];
+    }
+    void incpref() {
+        cntpref++;
+    }
+    void decpref() {
+        cntpref--;
+    }
+    void setend() {
+        endwith++;
+    }
+    void remend() {
+        endwith--;
+    }
+};
+class Trie{
+private:
+    Node* root;
+public:
+    Trie(){
+        root = new Node();
+    }
+    void insert(string &word){
+        Node* curr = root;
+        for (int i = 0; i < word.size(); i++) {
+            if (!curr->containsref(word[i])) {
+                curr->putref(word[i], new Node());
+            }
+            curr = curr->getref(word[i]);
+            curr->incpref();
+        }
+        curr->setend();
+    }
+
+    int countWordsEqualTo(string &word){
+        int count = 0;
+        Node* curr = root;
+        for (int i = 0; i < word.size(); i++) {
+            if (!curr->containsref(word[i])) {
+                return 0;
+            }
+            curr = curr->getref(word[i]);
+        }
+        return curr->endwith;
+    }
+
+    int countWordsStartingWith(string &word){
+        int count = 0;
+        Node* curr = root;
+        for (int i = 0; i < word.size(); i++) {
+            if (!curr->containsref(word[i])) {
+                return 0;
+            }
+            curr = curr->getref(word[i]);
+        }
+        return curr->cntpref;
+    }
+
+    void erase(string &word){
+        Node* curr = root;
+        for (int i = 0; i < word.size(); i++) {
+            curr = curr->getref(word[i]);
+            curr->decpref();
+        }
+        curr->remend();
+    }
+};
+
+
+// problem link -> https://www.naukri.com/code360/problems/implement-trie_1387095?leftPanelTabValue=PROBLEM
