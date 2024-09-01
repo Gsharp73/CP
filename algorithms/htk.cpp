f6fb450d3abf6ca52cf57187de511a72c380494d
Gsharp73 <tripathidhyanendra2004@gmail.com>
2024-09-01 12:32:35 +0530

    htk2

--- algorithms/htk.cpp
+++ algorithms/htk.cpp
@@ -10,6 +10,8 @@ public:
 
         for (int i = 0; i < n; i++) {
             double ratio = double(damage[i]) / ceil(double(health[i]) / power);
+            // if it gives more damage in ratio to the times taken to kill it then it should
+            // be killed first
             // use this ratio to detect who to kill first
             d.push_back(make_tuple(ratio, damage[i], ceil(double(health[i]) / power)));
         }
