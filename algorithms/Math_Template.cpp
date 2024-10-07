7ed6e9db57da8df671186f150e9cd569827ad93c
Gsharp73 <tripathidhyanendra2004@gmail.com>
2024-10-08 01:00:09 +0530

    maths template updated

--- algorithms/Math_Template.cpp
+++ algorithms/Math_Template.cpp
@@ -2,41 +2,44 @@
 using namespace std;
 #define ll long long
 
+const int MOD = 1e9 + 7;
+ 
+int mod(int a, int m = MOD) {
+    return a % m;
+}
+ 
 template <class T> class Math {
   public:
-    vector<T> fac;
-    vector<T> invfac;
-    Math() {
-
-    }
+    // Math<datatype> objname(n); use like this
+    vector<T> fact, invfact;
+    Math() {}
     Math(int n) {
-        fac.resize(n);
-        invfac.resize(n);
-        fac[0] = invfac[0] = 1;
+        fact.resize(n);
+        invfact.resize(n);
+        fact[0] = invfact[0] = 1;
         for (int i = 1; i < n; i++) {
-            fac[i] = mod(i * fac[i - 1]);
-            invfac[i] = modinv(fac[i]);
+            fact[i] = mod(i * fact[i - 1]);
+            invfact[i] = modinv(fact[i]);
         }
     }
-    T modinv(T x, T m = MOD) { return expo(x, m - 2, m); }
+    T modinv(T x, T m = MOD) { 
+        return expo(x, m - 2, m); 
+    }
     T expo(T base, T exp, T m = MOD) {
         T res = 1;
         while (exp) {
-            if (exp & 1) {
+            if (exp & 1)
                 res = mod(res * base, m);
-            }
             base = mod(base * base, m);
             exp >>= 1;
         }
         return res;
     }
     T choose(T n, T k) {
-        if (k < 0 || k > n) {
-            return 0;
-        }
-        T ans = fac[n];
-        ans = mod(ans * invfac[n - k]);
-        ans = mod(ans * invfac[k]);
+        if (k < 0 || k > n || n < 0) return 0;
+        T ans = fact[n];
+        ans = mod(ans * invfact[n - k]);
+        ans = mod(ans * invfact[k]);
         return ans;
     }
 };
