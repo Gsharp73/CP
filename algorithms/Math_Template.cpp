f616458154ec3baf5bef2c457b55694c5a073b1d
Gsharp73 <tripathidhyanendra2004@gmail.com>
2024-10-19 17:42:15 +0530

    maths template improved

--- algorithms/Math_Template.cpp
+++ algorithms/Math_Template.cpp
@@ -1,55 +1,70 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define ll long long
-
 const int MOD = 1e9 + 7;
- 
-int mod(int a, int m = MOD) {
-    return a % m;
-}
- 
-template <class T> class Math {
+template <class T> 
+class Math {
   public:
-    // Math<datatype> objname(n); use like this
     vector<T> fact, invfact;
+    // Math<datatype> objname(n); use like this
     Math() {}
     Math(int n) {
         fact.resize(n);
         invfact.resize(n);
         fact[0] = invfact[0] = 1;
         for (int i = 1; i < n; i++) {
-            fact[i] = mod(i * fact[i - 1]);
+            fact[i] = modmul(i, fact[i - 1]);
             invfact[i] = modinv(fact[i]);
         }
     }
-    T modinv(T x, T m = MOD) { 
-        return expo(x, m - 2, m); 
-    }
-    T expo(T base, T exp, T m = MOD) {
+
+    T binpow(T a, T b, T m = MOD) {
         T res = 1;
-        while (exp) {
-            if (exp & 1)
-                res = mod(res * base, m);
-            base = mod(base * base, m);
-            exp >>= 1;
+        while (b > 0) {
+            if (b & 1)
+                res = modmul(res, a, m);
+            a = modmul(a, a, m);
+            b >>= 1;
         }
         return res;
     }
+
+    T modadd(T a, T b, T m = MOD) {
+        a = a % m;
+        b = b % m;
+        return (((a + b) % m) + m) % m;
+    }
+
+    T modsub(T a, T b, T m = MOD) {
+        a = a % m;
+        b = b % m;
+        return (((a - b) % m) + m) % m;
+    }
+
+    T modmul(T a, T b, T m = MOD) {
+        a = a % m;
+        b = b % m;
+        return (((T)a * (T)b % m) + m) % m;
+    }
+
+    T modpow(T x, T y, T m = MOD) {
+        T res = 1;
+        x = x % m;
+        while (y > 0) {
+            if (y & 1)
+                res = (res * x) % m;
+            y = y >> 1;
+            x = (x * x) % m;
+        }
+        return res;
+    }
+
+    T modinv(T x, T m = MOD) {
+        return modpow(x, m - 2, m);
+    }
+
     T choose(T n, T k) {
-        if (k < 0 || k > n || n < 0) return 0;
+        if (k < 0 || k > n) return 0;
         T ans = fact[n];
-        ans = mod(ans * invfact[n - k]);
-        ans = mod(ans * invfact[k]);
+        ans = modmul(ans, invfact[k]);
+        ans = modmul(ans, invfact[n - k]);
         return ans;
     }
 };
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int tt; cin >> tt;
-    while (tt--) {
-
-    }
-}
-
